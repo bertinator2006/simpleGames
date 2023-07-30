cdbab9342a634436babe6126344b896d028f0204
bertinator2006 <53633607+bertinator2006@users.noreply.github.com>
2023-07-30 22:04:53 +1000

    Added Ball
    Ball bounces off sides, collisions with the paddles is yet to be implemented.

--- Pong/c/main.c
+++ Pong/c/main.c
@@ -1,17 +1,23 @@
 #include <SDL.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #undef main
 
 enum Direction {
 	up = -1, none = 0, down = 1
 };
-
 struct player
 {
 	SDL_Rect rect;
 	enum Direction direction;
-
+};
+struct Ball
+{
+	SDL_Rect rect;
+	float xvel;
+	float yvel;
 };
 
 bool init_sdl();
@@ -21,6 +27,8 @@ void close_game();
 void update_screen();
 void move_paddles();
 void init_game();
+int update_ball();
+void debug_statement(int value);
 
 SDL_Window* window = NULL;
 SDL_Renderer* renderer = NULL;
@@ -29,9 +37,12 @@ SDL_Event e;
 const int SCREEN_WIDTH = 850;
 const int SCREEN_HEIGHT = 500;
 const int GAME_SPEED = 500;
+const int TILE_SIZE = 10;
 
 struct player player[2];
+struct Ball ball;
 bool quit;
+bool game_start;
 int deltatime_ms;
 
 
@@ -54,9 +65,9 @@ int main()
 	while (!quit)
 	{
 		handle_input();
-
-		//Need to update the game state here
 		move_paddles();
+
+		update_ball(); //return value checking still to be done
 
 		update_screen();
 
@@ -72,18 +83,55 @@ int main()
 }
 
 
-void move_paddles()
+//(incomplete) moves ball for a particular frame, returns true if collision, false otherwise
+int update_ball()
 {
-	for (int i = 0; i < 2; i++)
-	{
-		player[i].rect.y += player[i].direction*GAME_SPEED*deltatime_ms/1000;
-		player[i].direction = none;
-	}
+	if (!game_start)
+		return 0;
 	
+	int deltax = ball.xvel * GAME_SPEED * deltatime_ms / 1000;
+	int deltay = ball.yvel * GAME_SPEED * deltatime_ms / 1000;
+	int nextx = ball.rect.x + deltax;
+	int nexty = ball.rect.y + deltay;
+
+	if (nexty < 0 || nexty + ball.rect.h > SCREEN_HEIGHT)
+	{
+		ball.yvel *= -1;
+		deltay *= -1;
+	}
+
+	ball.rect.x += deltax;
+	ball.rect.y += deltay;
+		
+	
+	return 0;
 }
 
+//(need to add random) Initialises both paddle rects, ball rect and ball velocities
+void init_game()
+{
+	srand(time(0));
+	ball.xvel = 0.5;
+	ball.yvel = 0.5;
 
-//(incomplete) Displays both items and ball
+	game_start = false;
+	ball.rect.w = TILE_SIZE * 1.5;
+	ball.rect.h = TILE_SIZE * 1.5;
+	ball.rect.x = SCREEN_WIDTH / 2 - ball.rect.w / 2;
+	ball.rect.y = SCREEN_HEIGHT / 2 - ball.rect.h / 2;
+
+	for (int i = 0; i < 2; i++)
+	{
+		player[i].rect.w = TILE_SIZE / 2;
+		player[i].rect.h = TILE_SIZE * 8;
+		player[i].rect.x = i * (SCREEN_WIDTH - player[i].rect.w) - 10 * (i * 2 - 1);
+		player[i].rect.y = SCREEN_HEIGHT / 2 - player[i].rect.h / 2;
+		player[i].direction = none;
+		printf("Player %d:\n	x:%d\n	y:%d\n	w:%d\n	h:%d\n", i + 1, player[i].rect.x, player[i].rect.y, player[i].rect.h, player[i].rect.w);
+	}
+}
+
+//Displays both items and ball
 void update_screen()
 {
 	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
@@ -92,6 +140,7 @@ void update_screen()
 	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 	SDL_RenderFillRect(renderer, &player[0].rect);
 	SDL_RenderFillRect(renderer, &player[1].rect);
+	SDL_RenderFillRect(renderer, &ball.rect);
 
 	SDL_RenderPresent(renderer);
 }
@@ -142,6 +191,8 @@ void handle_input()
 		player[0].direction = down;
 	if (keystate[SDL_SCANCODE_ESCAPE])
 		quit = true;
+	if (keystate[SDL_SCANCODE_SPACE])
+		game_start = true;
 }
 
 //Called whenever there is an error in the code
@@ -161,16 +212,24 @@ void close_game()
 	SDL_Quit();
 }
 
-// loops through both players and sets their initial vales
-void init_game()
+//Moves the paddles depending on direction of travel
+void move_paddles()
 {
+	int new_y_pos;
 	for (int i = 0; i < 2; i++)
 	{
-		player[i].rect.w = 5;
-		player[i].rect.h = 80;
-		player[i].rect.x = i * (SCREEN_WIDTH - player[i].rect.w) - 10 * (i * 2 - 1);
-		player[i].rect.y = SCREEN_HEIGHT / 2 - player[i].rect.h / 2;
+		new_y_pos = player[i].rect.y + player[i].direction * GAME_SPEED * deltatime_ms / 1000;
+
+		if (new_y_pos >= 0 && new_y_pos + player[i].rect.h <= SCREEN_HEIGHT)
+			player[i].rect.y = new_y_pos;
+
 		player[i].direction = none;
-		printf("Player %d:\n	x:%d\n	y:%d\n	w:%d\n	h:%d\n", i + 1, player[i].rect.x, player[i].rect.y, player[i].rect.h, player[i].rect.w);
 	}
+
+}
+
+//(temporary) prints debug statement
+void debug_statement(int value)
+{
+	printf("The value given is %d\n", value);
 }
